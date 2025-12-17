cb13a430bee112cda6d6be2aac71e441c6815535
Ulil Fahmi <ulilf442@gmail.com>
2025-12-17 16:42:30 +0700

    pubspec, gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,12 +29,19 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        testInstrumentationRunner = "dev.patrol.runner.PatrolTestRunner"
+    }
+
+    packagingOptions {
+        jniLibs {
+            useLegacyPackaging = true
+        }
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so flutter run --release works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
